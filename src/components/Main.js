706c27f016e6532ad79bdd5487fcc25185fe634d
Adarshcodes <adarsh.lrnr@gmail.com>
2022-01-26 23:05:34 +0530

    completed the first react project

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,20 @@ import React from "react";
 import "../style.css";
 
 function Main() {
-	return <h1>This is the main component</h1>;
+	return (
+		<div className="main-section">
+			<h1 className="heading-primary">Fun facts about React</h1>
+			<ul className="main-list">
+				<li className="main-item">Was first released in 2013</li>
+				<li className="main-item">Was originally created by Jordan Walke</li>
+				<li className="main-item">Has well over 100K stars on GitHub</li>
+				<li className="main-item">Is maintained by Facebook</li>
+				<li className="main-item">
+					Powers thousands of enterprise apps, including mobile apps
+				</li>
+			</ul>
+		</div>
+	);
 }
 
 export default Main;
