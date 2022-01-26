43f43628de7c720c5e84078a5733f06ff6983a2d
Adarshcodes <adarsh.lrnr@gmail.com>
2022-01-26 22:09:01 +0530

    completed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,33 @@
 import React from "react";
 // adding CSS files
 import "../style.css";
-
-export default class Navbar extends React.Component {
+import logo from "../images/react-icon-small.png";
+class LogoBox extends React.Component {
 	render() {
-		return <h1>This is the navigation.</h1>;
+		return (
+			<div className="logoBox">
+				<img src={logo} alt="logo" className="logo" />
+				<h3 className="logo-heading">ReactFacts</h3>
+			</div>
+		);
 	}
 }
+
+class NavText extends React.Component {
+	render() {
+		return <h4 className="navigation-text"> React Course - Project 1</h4>;
+	}
+}
+
+class NavBar extends React.Component {
+	render() {
+		return (
+			<div className="navbar">
+				<LogoBox />
+				<NavText />
+			</div>
+		);
+	}
+}
+
+export default NavBar;
